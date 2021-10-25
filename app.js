886d7877a6f8ab763e7d3e68216bd0ef6726a099
supriyayadav221 <supriyayadav221@gmail.com>
2021-10-25 15:43:20 +0530

    rest-api

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const PORT = process.env.PORT || 3000;
+
+//middleswares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("HomePage");
+});
+const postsRoute = require("./routes/posts");
+app.use("/posts", postsRoute);
+
+//Connect to Db
+
+mongoose.connect(process.env.MONGO_URI, () =>
+  console.log("Connected to Db!")
+);
+
+app.listen(PORT);
